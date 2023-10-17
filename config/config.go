26470865ada8aab5200b01b8a90c86eeb455bc36
Vladimir Belyy <whitevl41@gmail.com>
2023-10-17 21:56:35 +0300

    config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+}
+
+// ParseConfig Parse config file
+func GetConfig() (*Config, error) {
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	viper.SetConfigName("config.yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var c Config
+	if err := viper.Unmarshal(&c); err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
