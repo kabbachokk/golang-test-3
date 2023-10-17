4993d2c9a1ff964a46eaab4602cfc7ab4ba4a52a
Vladimir Belyy <whitevl41@gmail.com>
2023-10-17 22:02:57 +0300

    console

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"ip.com/app/delivery/console"
+	"ip.com/config"
+)
+
+func main() {
+	c, err := config.GetConfig()
+	if err != nil {
+		log.Print(err)
+	}
+	log.Print(c)
+	console.Execute()
+}
