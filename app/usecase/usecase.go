66b226d4ec2b06a2c55fa13cfc31e91e8816c6a6
Vladimir Belyy <whitevl41@gmail.com>
2023-10-17 23:33:10 +0300

    uc

--- app/usecase/usecase.go
+++ app/usecase/usecase.go
@@ -1,1 +1,17 @@
 package usecase
+
+import (
+	"ip.com/app"
+)
+
+// useCase
+type useCase struct {
+	repo app.MysqlRepository
+}
+
+// NewProductUC constructor
+func NewUseCase(
+	repo app.MysqlRepository,
+) *useCase {
+	return &useCase{repo}
+}
--- main.go
+++ main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"log"
+	"os"
 
-	"ip.com/app/delivery/console"
+	"ip.com/cmd"
 	"ip.com/config"
+	"ip.com/db"
 )
 
 func main() {
-	c, err := config.GetConfig()
+	conf, err := config.GetConfig()
 	if err != nil {
 		log.Print(err)
+		os.Exit(1)
 	}
-	log.Print(c)
-	console.Execute()
+
+	log.Print(conf)
+
+	conn, err := db.NewMysqlConn(conf)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	if err := cmd.Execute(); err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
