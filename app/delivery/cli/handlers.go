cc781b7720dccec6436a3b95c06c9cf7adb68769
Vladimir Belyy <whitevl41@gmail.com>
2023-10-18 00:01:41 +0300

    cli

--- app/delivery/cli/handlers.go
+++ app/delivery/cli/handlers.go
@@ -1,15 +1,20 @@
 package cli
 
 import (
+	"github.com/spf13/cobra"
 	"ip.com/app"
 )
 
 type cliHandlers struct {
 	uc app.UseCase
+	rc *cobra.Command // rootCmd
 }
 
 func NewCliHandlers(
 	uc app.UseCase,
+	rc *cobra.Command,
 ) *cliHandlers {
-	return &cliHandlers{uc}
+	return &cliHandlers{uc, rc}
 }
+
+func (p *cliHandlers) Handler(cmd *cobra.Command, args []string) {}
